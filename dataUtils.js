341548bbd42d212664281779c32977f9821b908e
Otto Virkki <otto.virkki@gmail.com>
2015-10-25 14:34:32 +0200

    basic listing of data

--- dataUtils.js
+++ dataUtils.js
@@ -1,19 +1,63 @@
+var dataFileAddress = "dataFile.json";
+
 function readFile() {
-    $("#randoms .dataBox").text("Text from code");
+    $.ajax({
+        url: dataFileAddress,
+        dataType: "json",
+        success: handleData
+    });
 }
 
-function parseData(data) {
+function handleData(data) {
     console.log("All data: " + JSON.stringify(data));
     if(data === undefined) {
     	console.log("Empty data");
     	return;
-    } 
+    }
     completeData = data;
-    var randoms = data.randoms;
-    var blocks = data.blocks;
-    displayData(data);
 
-	$("#randoms .dataBox").text(JSON.stringify(randoms));
+    renderData(data);
+}
+
+function renderNotes(notes) {
+    var noteListElement = $("<ul></ul>").addClass("notelist");
+    var notesAsHtmlElementList = Object.keys(notes).map(function(noteId) {
+        var noteText = notes[noteId].freeText;
+        if(noteText === undefined) {
+            return "Text not found";
+        }
+        return "<li>" + noteText + "</li>";
+    });
+    return noteListElement.append(notesAsHtmlElementList);
+}
+
+function renderOnePicture(key, noteData) {
+    var pictureElement = $("<li></li>").addClass("pictureItem").attr("id", "picture-" + key).html(key);
+    pictureElement.append(renderNotes(noteData));
+    var htmlCode = "<div>test data</div>";
+    //$(elementId).html(htmlCode);
+    //return "key: " + key;
+    return pictureElement;
+}
+
+function isValidData(data) {
+    console.log("is valid: " + JSON.stringify(data));
+    return data !== undefined &&
+        data.notes !== undefined &&
+        Object.keys(data.notes).length >= 1;
+}
+
+function renderData(data) {
+    $(".dataBox").append("<ul class=\"pictureList\"></ul>");
+    Object.keys(data).filter(function(key) {
+        return isValidData(data[key]);
+    })
+    .forEach(function(key) {
+        console.log("foreach for key: " + key);
+        $(".pictureList").append(renderOnePicture(key, data[key].notes))
+    })
+    //renderOnePicture(data, "#randoms .dataBox");
+    //$("#randoms .dataBox").text(JSON.stringify(data));
 }
 
 function getFile() {
